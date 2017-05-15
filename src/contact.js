6b8a096ce5287987df33de87e8fe1b84471c6783
Puneet Kashyap <puneet.kashyap@loyalty.com>
2017-05-15 16:53:30 -0400

    working copy of the home staging with chat bot

--- src/contact.js
+++ src/contact.js
@@ -8,51 +8,10 @@ class Contact extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12 text-center">
-                            <h2>Contact Me</h2>
+                            <h2>Let's Chat.</h2>
                             <hr className="star-primary"></hr>
+                            <iframe width="100%" height="500" src="https://console.api.ai/api-client/demo/embedded/6b41f18a-0072-4a61-8829-15e899fe64ae"></iframe>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-8 col-lg-offset-2">
-                            <form name="sentMessage" id="contactForm">
-                                <div className="row control-group">
-                                    <div className="form-group col-xs-12 floating-label-form-group controls">
-                                        <label>Name</label>
-                                        <input type="text" className="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name."></input>
-                                            <p className="help-block text-danger"></p>
-                                    </div>
-                                </div>
-                                <div className="row control-group">
-                                    <div className="form-group col-xs-12 floating-label-form-group controls">
-                                        <label>Email Address</label>
-                                        <input type="email" className="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address."></input>
-                                            <p className="help-block text-danger"></p>
-                                    </div>
-                                </div>
-                                <div className="row control-group">
-                                    <div className="form-group col-xs-12 floating-label-form-group controls">
-                                        <label >Phone Number</label>
-                                        <input type="tel" className="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number."></input>
-                                            <p className="help-block text-danger"></p>
-                                    </div>
-                                </div>
-                                <div className="row control-group">
-                                    <div className="form-group col-xs-12 floating-label-form-group controls">
-                                        <label>Message</label>
-                                        <textarea rows="5" className="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                                        <p className="help-block text-danger"></p>
-                                    </div>
-                                </div>
-                                <br></br>
-                                    <div id="success"></div>
-                                    <div className="row">
-                                        <div className="form-group col-xs-12">
-                                            <button type="submit" className="btn btn-success btn-lg">Send</button>
-                                        </div>
-                                    </div>
-                            </form>
-
-                            </div>
                     </div>
                 </div>
             </section>
