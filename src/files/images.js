31980891be61a607c9f8c415bf4efed02c9e19d0
Puneet Kashyap <pkashyaps@gmail.com>
2017-06-26 14:56:56 -0400

    added images of rainforest and refactored images.js

--- src/files/images.js
+++ src/files/images.js
@@ -1,26 +1,56 @@
-export const prudhommeImages = [
-      {
-        original: require('./../img/Prudhomme/IMG_0009.jpg'),
-        thumbnail: require('./../img/Prudhomme/IMG_0009.jpg'),
-      },
-      {
-        original: require('./../img/Prudhomme/IMG_0012.jpg'),
-        thumbnail: require('./../img/Prudhomme/IMG_0012.jpg'),
-      },
-      {
-        original: require('./../img/Prudhomme/IMG_0013.jpg'),
-        thumbnail: require('./../img/Prudhomme/IMG_0013.jpg'),
-      },
-      {
-        original: require('./../img/Prudhomme/IMG_0024.jpg'),
-        thumbnail: require('./../img/Prudhomme/IMG_0024.jpg'),
-      },
-      {
-        original: require('./../img/Prudhomme/IMG_0029.jpg'),
-        thumbnail: require('./../img/Prudhomme/IMG_0029.jpg'),
-      },
-      {
-        original: require('./../img/Prudhomme/IMG_0033.jpg'),
-        thumbnail: require('./../img/Prudhomme/IMG_0033.jpg'),
-      }
+    let prudhommePix = [
+      require('./../img/Portfolio/Prudhomme/IMG_0009.jpg'),
+      require('./../img/Portfolio/Prudhomme/IMG_0012.jpg'),
+      require('./../img/Portfolio/Prudhomme/IMG_0013.jpg'),
+      require('./../img/Portfolio/Prudhomme/IMG_0024.jpg'),
+      require('./../img/Portfolio/Prudhomme/IMG_0029.jpg'),
+      require('./../img/Portfolio/Prudhomme/IMG_0033.jpg'),
     ]
+    export const prudhommeImages = []
+    prudhommePix.forEach((item) => prudhommeImages.push({original:item, thumbnail:item}))
+
+    let rainforestPix= [
+      require('./../img/Portfolio/Rainforest/IMG_0001.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0002.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0003.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0004.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0005.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0006.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0007.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0008.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0009.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0010.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0011.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0012.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0013.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0014.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0015.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0016.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0017.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0018.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0019.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0020.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0021.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0022.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0023.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0024.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0025.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0026.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0027.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0028.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0029.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0030.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0031.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0032.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0033.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0034.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0035.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0036.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0037.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0038.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0039.jpg'),
+      require('./../img/Portfolio/Rainforest/IMG_0040.jpg')
+    ]
+
+    export const rainforestImages = []
+    rainforestPix.forEach((item) => rainforestImages.push({original:item, thumbnail:item}))
