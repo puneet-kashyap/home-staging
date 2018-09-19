6e03963ab9ee662f2c47f730b9880cc7e584423c
Puneet Kashyap <pkashyaps@gmail.com>
2018-09-19 09:35:20 -0400

    Cloud function validation added to check for change in image folder only

--- functions/index.js
+++ functions/index.js
@@ -12,9 +12,13 @@ exports.retrieveStorage = functions.storage.object().onFinalize((object) => {
         const fileName = name[2].split('.')[0];
         const downloadableUrl = `https://firebasestorage.googleapis.com/v0/b/${object.bucket}/o/${encodeURIComponent(object.name)}?alt=media&token=${process.env.mediaToken}`;
         // Saving downloadable url to db
-        galleryRef.child(folderName).update({
-            [fileName] : downloadableUrl
-        });
+        if (object.contentType.startsWith('image/')) {
+            galleryRef.child(folderName).update({
+                [fileName]: downloadableUrl
+            });
+        } else {
+            console.log(`Non image file added ${fileName}`);
+        }
         console.log(object);
     }
 });
@@ -25,7 +29,9 @@ exports.onDeleteImages = functions.storage.object().onDelete((object) => {
         const folderName = name[1];
         const fileName = name[2].split('.')[0];
         //deletes the db reference when image is removed
-        galleryRef.child(folderName).child(fileName).remove();
+        if (object.contentType.startsWith('image/')) {
+            galleryRef.child(folderName).child(fileName).remove();
+        }
         console.log(object);
     };
 })
